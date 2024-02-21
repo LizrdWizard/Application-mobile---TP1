da954d693f382d9ff83f87e0d90e9417eda0034e
Jasmin <Jasmin@FRANKLIN2>
2024-02-21 15:17:46 -0500

    ajout commentaires javadoc

--- src/PixelNoirBlanc.java
+++ src/PixelNoirBlanc.java
@@ -10,8 +10,8 @@ public class PixelNoirBlanc implements Pixel {
     }
 
     /**
-     * @param       :
-     * @description :
+     * @param       : Object Scanner en cours de lecture du fichier
+     * @description : Le pixel lit le prochain mot dans le fichier et ajoute sa valeur à _pixel si c'est un short
      */
     public void lire(Scanner scanner) {
 
@@ -29,16 +29,15 @@ public class PixelNoirBlanc implements Pixel {
     }
 
     /**
-     * @param       :
-     * @description :
+     * @description : Retourne la valeur de _pixel
      */
     public String ecrire() {
         return String.valueOf(_pixel) + ' ';
     }
 
     /**
-     * @param       :
-     * @description :
+     * @param       : Short valeur représentant les unités de luminosité à ajuster
+     * @description : Change la valeur de _pixel pour ajuster la luminosité de l'image
      */
     public void eclaircir_noircir(short valeur) {
 
@@ -48,6 +47,10 @@ public class PixelNoirBlanc implements Pixel {
         if (_pixel > 255) {_pixel = 255;}
     }
 
+    /**
+     * @param       : Liste de Pixels
+     * @description : Retourne la couleur moyenne de tous les Pixels dans l'image
+     */
     public Pixel moyenne(List<Pixel> listePixels) {
         PixelNoirBlanc pixelMoyen = this;
 
